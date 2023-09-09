19b30f356f483b1ba8516e0812dd36e2d11a0c19
Linc_li <linc_li@sina.cn>
2023-09-09 22:42:20 +0800

    add demo code

---
+++ concurrency-in-practise/src/main/java/com/concurrency/boundbuffer/SemaphoreOnLock.java
@@ -0,0 +1,46 @@
+package com.concurrency.boundbuffer;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+@ThreadSafe
+public class SemaphoreOnLock {
+    private final Lock lock = new ReentrantLock();
+    private final Condition permitsAvailable = lock.newCondition();
+    @GuardedBy("lock")
+    private int permits;
+
+    SemaphoreOnLock(int initialPermits) {
+        lock.lock();
+        try {
+            permits = initialPermits;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void acquire() throws InterruptedException {
+        lock.lock();
+        try {
+            while (permits <= 0)
+                permitsAvailable.await();
+            --permits;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void release() {
+        lock.lock();
+        try {
+            ++permits;
+            permitsAvailable.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
